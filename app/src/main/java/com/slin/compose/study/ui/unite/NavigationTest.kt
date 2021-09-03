393cb8391d8432a44369847f4163c13a915f70f1
slin_huibo <dxslin@qq.com>
2021-09-03 15:29:04 +0800

    [study] BottomNavigationTest

--- app/src/main/java/com/slin/compose/study/ui/unite/NavigationTest.kt
+++ app/src/main/java/com/slin/compose/study/ui/unite/NavigationTest.kt
@@ -1,17 +1,30 @@
 package com.slin.compose.study.ui.unite
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Dashboard
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.rememberPagerState
 import com.slin.compose.study.R
 import com.slin.compose.study.ui.LocalNavController
 import com.slin.compose.study.ui.NavDestinations
 import com.slin.compose.study.ui.samples.LayoutItem
 import com.slin.compose.study.ui.samples.MultiTestPage
+import kotlinx.coroutines.launch
 
 /**
  * author: slin
@@ -25,7 +38,10 @@ import com.slin.compose.study.ui.samples.MultiTestPage
 @Composable
 fun NavigationTest() {
 
-    val testItems = listOf(LayoutItem("Navigation") { NavigationToPage() })
+    val testItems = listOf(
+        LayoutItem("Navigation") { NavigationToPage() },
+        LayoutItem("BottomNavigationTest") { BottomNavigationTest() },
+    )
 
     MultiTestPage(title = "1. NavigationTest", testItems = testItems)
 }
@@ -48,5 +64,51 @@ private fun NavigationToPage() {
             Text(text = "nav to theme")
         }
     }
+
+}
+
+
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+private fun BottomNavigationTest() {
+    val pagerState = rememberPagerState(pageCount = 3)
+    val coroutineScope = rememberCoroutineScope()
+    val animateToPage: (Int) -> Unit = { page ->
+        coroutineScope.launch { pagerState.animateScrollToPage(page) }
+    }
+
+    Column(modifier = Modifier.height(300.dp)) {
+        val colors = listOf(Color.Magenta, Color.Yellow, Color.Cyan)
+        HorizontalPager(state = pagerState, modifier = Modifier.weight(1f)) {
+            Text(
+                text = "当前处于第${pagerState.currentPage}页",
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(colors[pagerState.currentPage])
+            )
+        }
+
+        BottomNavigation(modifier = Modifier.fillMaxWidth()) {
+            BottomNavigationItem(selected = true, onClick = { animateToPage(0) }, icon = {
+                Icon(
+                    imageVector = Icons.Default.Home,
+                    contentDescription = "Home"
+                )
+            })
+            BottomNavigationItem(selected = true, onClick = { animateToPage(1) }, icon = {
+                Icon(
+                    imageVector = Icons.Default.Dashboard,
+                    contentDescription = "Dashboard"
+                )
+            })
+            BottomNavigationItem(selected = true, onClick = { animateToPage(2) }, icon = {
+                Icon(
+                    imageVector = Icons.Default.Notifications,
+                    contentDescription = "Notifications"
+                )
+            })
+        }
+    }
+
 
 }

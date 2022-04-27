6713b67420c8a1cac7d3c7d7a099b366baf496c8
slin_huibo <dxslin@qq.com>
2022-04-27 14:24:11 +0800

    [dev] 升级依赖库版本，添加注释

--- buildSrc/src/main/java/com/slin/compose/buildSrc/Dependencies.kt
+++ buildSrc/src/main/java/com/slin/compose/buildSrc/Dependencies.kt
@@ -12,13 +12,13 @@ object Versions {
 }
 
 object Libs {
-    const val androidGradlePlugin = "com.android.tools.build:gradle:7.1.0-alpha13"
+    const val androidGradlePlugin = "com.android.tools.build:gradle:7.1.1"
 
     const val timber = "com.jakewharton.timber:timber:4.7.1"
 
     object Slin {
         private const val snapshot = "score_snapshot"
-        private const val version = "1.1.1-alpha01"
+        private const val version = "1.1.3"
         const val score = "io.github.dxslin:Score:$version"
     }
 
@@ -31,7 +31,7 @@ object Libs {
      * doc: https://google.github.io/accompanist/
      */
     object Accompanist {
-        private const val version = "0.19.0"
+        private const val version = "0.23.1"
         const val insets = "com.google.accompanist:accompanist-insets:$version"
         const val theme = "com.google.accompanist:accompanist-appcompat-theme:$version"
         const val systemUiController =
@@ -48,18 +48,19 @@ object Libs {
     }
 
     object Kotlin {
-        private const val version = "1.5.21"
+        private const val version = "1.6.21"
         const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$version"
         const val gradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$version"
         const val extensions = "org.jetbrains.kotlin:kotlin-android-extensions:$version"
+
+        object Coroutines {
+            private const val version = "1.6.1"
+            const val core = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$version"
+            const val android = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$version"
+            const val test = "org.jetbrains.kotlinx:kotlinx-coroutines-test:$version"
+        }
     }
 
-    object Coroutines {
-        private const val version = "1.5.1"
-        const val core = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$version"
-        const val android = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$version"
-        const val test = "org.jetbrains.kotlinx:kotlinx-coroutines-test:$version"
-    }
 
     object JUnit {
         private const val version = "4.13"
@@ -67,13 +68,27 @@ object Libs {
     }
 
     object AndroidX {
-        const val coreKtx = "androidx.core:core-ktx:1.6.0"
-        const val material = "com.google.android.material:material:1.4.0"
-        const val appcompat = "androidx.appcompat:appcompat:1.3.1"
+        /**
+         * https://developer.android.google.cn/jetpack/androidx/releases/core
+         */
+        const val coreKtx = "androidx.core:core-ktx:1.7.0"
 
+        /**
+         * https://github.com/material-components/material-components-android/releases
+         */
+        const val material = "com.google.android.material:material:1.5.0"
+
+        /**
+         * https://developer.android.google.cn/jetpack/androidx/releases/appcompat
+         */
+        const val appcompat = "androidx.appcompat:appcompat:1.4.1"
+
+        /**
+         * https://developer.android.google.cn/jetpack/androidx/releases/compose
+         */
         object Compose {
             const val snapshot = ""
-            const val version = "1.0.1"
+            const val version = "1.1.1"
 
             const val animation = "androidx.compose.animation:animation:$version"
             const val foundation = "androidx.compose.foundation:foundation:$version"
@@ -92,17 +107,39 @@ object Libs {
 
         }
 
+        /**
+         * https://developer.android.google.cn/jetpack/androidx/releases/activity
+         */
         object Activity {
-            const val activityCompose = "androidx.activity:activity-compose:1.3.1"
+            const val activityCompose = "androidx.activity:activity-compose:1.4.0"
         }
 
+        /**
+         * https://developer.android.google.cn/jetpack/androidx/explorer
+         */
         object Jetpack {
-            private const val lifecycleVersion = "2.4.0-alpha03"
-            private const val hiltVersion = "2.38.1"
-            private const val hiltViewModelVersion = "1.0.0-alpha02"
-            private const val roomVersion = "2.2.5"
-            private const val pagingComposeVersion = "1.0.0-alpha12"
-            private const val pagingVersion = "3.1.0-alpha03"
+            /**
+             * https://developer.android.google.cn/jetpack/androidx/releases/lifecycle
+             */
+            private const val lifecycleVersion = "2.4.1"
+
+            /**
+             * https://dagger.dev/hilt/gradle-setup
+             * https://developer.android.google.cn/jetpack/androidx/releases/hilt
+             */
+            private const val hiltVersion = "2.41"
+            private const val hiltViewModelVersion = "1.0.0"
+
+            /**
+             * https://developer.android.google.cn/jetpack/androidx/releases/room
+             */
+            private const val roomVersion = "2.4.2"
+
+            /**
+             * https://developer.android.google.cn/jetpack/androidx/releases/paging
+             */
+            private const val pagingComposeVersion = "1.0.0-alpha14"
+            private const val pagingVersion = "3.1.1"
 
 
             const val lifecycle = "androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion"
@@ -128,14 +165,20 @@ object Libs {
 
         }
 
+        /**
+         * https://developer.android.google.cn/jetpack/androidx/releases/navigation
+         */
         object Navigation {
-            private const val version = "2.3.5"
+            private const val version = "2.4.2"
             const val fragment = "androidx.navigation:navigation-fragment-ktx:$version"
             const val uiKtx = "androidx.navigation:navigation-ui-ktx:$version"
 
             const val navigation = "androidx.navigation:navigation-compose:2.4.0-alpha06"
         }
 
+        /**
+         * https://developer.android.google.cn/jetpack/androidx/releases/constraintlayout
+         */
         object ConstraintLayout {
             const val constraintLayoutCompose =
                 "androidx.constraintlayout:constraintlayout-compose:1.0.0-beta02"
